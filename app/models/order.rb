1dc45a5c1e46b5059564d568371101af240eeacc
Lauren Dove <elledoveinc@gmail.com>
2020-10-27 15:29:27 -0400

    added two methods to the order model. one to add up total cost of menu items and another to keep track of all orders that come in

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,22 @@ class Order < ApplicationRecord
 
 
     validates :name_for_order, presence: true
+    #this validation will make sure that the order has the menu_item_id attached to each order.
+    validates :menu_item_ids, presence: true
+
+   #This method will keep track of how may orders come through the system.
+    def self.order_count
+        Order.all.count
+    end
+
+     #This method will give us the total from our menu_items 
+    def total
+        total = 0
+        self.menu_items.each do |item|
+          total += item.price
+        end
+        "$#{total}"
+      end
+
+
 end
