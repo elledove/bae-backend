9c4ad10f0e40dcfdc1432f8d8ec192f82b6b7853
Lauren Dove <elledoveinc@gmail.com>
2020-10-27 14:39:07 -0400

    created seed data to test models, also install faker gem to generate fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# Test user
+User.create!(name:"Boss", email: "youbettawerk@gmail.com", password:"password")
+
+
+#Test menu meals
+m1 = MenuItem.create(name: "Salmon Platter", description:"Glazed Salmon w/ asparagus and a side salad", price:13.50)
+m1 =MenuItem.create(name:"Bangin'Lasagna" ,description: "Homemade Lasagna with garlic bread and a side ceasar side salad", price: 15.00)
+d1 =MenuItem.create(name: "banna pudding", description: "Heavenly banana pudding with whip creme to finish", price:5.00)
+b1 = MenuItem.create(name:"strawberry lemonade", description:"homemade and homegrown ingredients, no extra sugar added", price:3.75)
+
+# Test orders
+
+order1 = Order.create!(user_id:1, total: 20, name_for_order: Faker::Name.first_name, menu_item_ids:[Faker::Number.within(range: 1..4), Faker::Number.within(range: 1..4)] )
